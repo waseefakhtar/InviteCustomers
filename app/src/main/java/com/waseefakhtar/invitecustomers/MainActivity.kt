53b575d83ae9f499af224fa1979b92a6369b5cea
Waseef Akhtar <waseef@degoo.com>
2020-01-25 18:45:46 +0500

    Solving the problem with law of cosines

--- app/src/main/java/com/waseefakhtar/invitecustomers/MainActivity.kt
+++ app/src/main/java/com/waseefakhtar/invitecustomers/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.waseefakhtar.invitecustomers
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
 import com.waseefakhtar.invitecustomers.data.Customer
@@ -9,6 +8,10 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.io.Reader
+
+private const val INTERCOM_LAT = 53.339428
+private const val INTERCOM_LONG = -6.257664
+private const val DISTANCE_CAP = 100.0
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +22,13 @@ class MainActivity : AppCompatActivity() {
 
         val customerList = getAllCustomers()
 
+        for (customer in customerList) {
+            val distance = getDistance(customer.longitude.toDouble(), customer.latitude.toDouble())
 
-        
+            if (distance <= DISTANCE_CAP) {
+                println("${customer.name} is invited! Distance: ${distance}")
+            }
+        }
     }
 
     private fun getAllCustomers(): ArrayList<Customer> {
@@ -44,5 +52,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         return arrayListOf()
+    }
+
+    private fun getDistance(customerLongitude: Double, customerLatitude: Double): Double {
+        val x1 = Math.toRadians(INTERCOM_LONG)
+        val y1 = Math.toRadians(INTERCOM_LAT)
+        val x2 = Math.toRadians(customerLongitude)
+        val y2 = Math.toRadians(customerLatitude)
+
+        /*************************************************************************
+         * Compute using law of cosines
+         *************************************************************************/
+        // great circle distance in radians
+        /*************************************************************************
+         * Compute using law of cosines
+         */
+
+        // great circle distance in radians
+        var angle1 = Math.acos(
+            Math.sin(x1) * Math.sin(x2)
+                    + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2)
+        )
+
+        // convert back to degrees
+        // convert back to degrees
+        angle1 = Math.toDegrees(angle1)
+
+        // each degree on a great circle of Earth is 60 nautical miles
+        // each degree on a great circle of Earth is 60 nautical miles
+        val distance1 = 60 * angle1
+
+        println("$distance1 nautical miles")
+
+        val distanceInKm = distance1 * 1.852
+
+        println("$distanceInKm kms")
+
+        return distanceInKm
     }
 }
